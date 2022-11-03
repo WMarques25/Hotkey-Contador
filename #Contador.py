5175e45b55f31409e4948346bf281bb0f07c4121
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2022-11-03 17:22:03 -0300

    NOVO

---
+++ #Contador.py
@@ -0,0 +1,43 @@
+#Contador
+# -*- coding: UTF-8 -*-
+from pynput import keyboard
+
+def on_activate():
+    print('Global hotkey activated!')
+    file = open("Contador.txt","r")
+    contx = file.readline()
+    print(contx)
+    file.close()
+    #file2 = open("Contador.txt","w")
+    contx = int(contx)
+    contx = 1 + contx
+    print(contx)
+    #file.write(contx)
+    #file.close()
+
+def for_canonical(f):
+    return lambda k: f(l.canonical(k))
+
+#with open("Hotkey.txt", "r") as text_file:
+#    data = text_file.readlines()
+#HKs = data[1] - para pegar a segunda linha.
+
+try:
+    file = open("Contador.txt","x")
+    file.close()
+    file2 = open("Contador.txt","w")
+    file2.write('0')
+    file2.close()
+finally:
+    HK = open("Hotkey.txt","r")
+    HKs = HK.readline()
+    HK.close()
+    print("Hotkey gravada é: " + HKs)
+
+    hotkey = keyboard.HotKey(
+        keyboard.HotKey.parse(HKs),
+        on_activate)
+    with keyboard.Listener(
+            on_press=for_canonical(hotkey.press),
+            on_release=for_canonical(hotkey.release)) as l:
+        l.join()
