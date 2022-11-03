257a021cdc94ab6ef0187fc9f925d697d51887f5
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2022-11-03 17:23:48 -0300

    novo

---
+++ #hotkey in.py
@@ -0,0 +1,16 @@
+#hotkey in
+# -*- coding: UTF-8 -*-
+from pynput import keyboard
+
+print("Digite a Hotkey que deseja gravar.\nUse o modelo : ' <ctrl>+<shift>+x '")
+
+
+Hotkey = input()
+
+print(Hotkey)
+HotkeyFile = open("Hotkey.txt", "w")
+
+HotkeyFile.write(Hotkey)
+
+HotkeyFile.close()
+
