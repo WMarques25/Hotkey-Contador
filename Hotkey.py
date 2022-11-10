aaee4970285ec289f8f7572a910a1b91e04281d1
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2022-11-10 14:04:34 -0300

    Update Hotkey.py
    Hotkey para zerar a contagem.

--- Hotkey.py
+++ Hotkey.py
@@ -1,18 +1,50 @@
 # Hotkey
 # -*- coding: UTF-8 -*-
+from os import system
 from pynput import keyboard
 
-# Gravando Hotkey +
-print("Digite a Hotkey que deseja gravar para aumentar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
-Hotkey = input()
-print("Hotkey gravada = ",Hotkey)
-HotkeyFile = open("Hotkey.txt", "w")
-HotkeyFile.write(Hotkey)
+try:
+    open("Hotkey.txt", "x")
 
-# Gravando Hotkey -
-print("Digite a Hotkey que deseja gravar para reduzir a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
-Hotkey2 = input()
-print("Hotkey gravada = ",Hotkey2)
-HotkeyFile.write("\n" + Hotkey2)
-HotkeyFile.close()
+except:
+    with open('Hotkey.txt','r') as f:
+        HKp = f.readline()
+        HKm = f.readline()
+        HKz = f.readline()
+    HKp = HKp[:-1]  #Limpando o '\n' das linhas em "Hotkey.txt"
+    HKm = HKm[:-1]
 
+    print("AS Hotkeys gravadas são:\n" + HKp + " para +\n" + HKm + " para -\n" + HKz + " para zerar. \n")
+    x = input("Deseja alterar as Hotkeys salvas? ('S' p/ SIM)\n\t")
+    
+else:
+    x = 'S'
+
+finally:
+    while x.upper() == 'S':
+        system('cls')
+        
+        # Gravando Hotkey +
+        print("Digite a Hotkey que deseja gravar para aumentar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+        Hotkey = input()
+        print("Hotkey gravada = ",Hotkey)
+        HotkeyFile = open("Hotkey.txt", "w")
+        HotkeyFile.write(Hotkey)
+
+        # Gravando Hotkey -
+        print("\nDigite a Hotkey que deseja gravar para reduzir a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+        Hotkey2 = input()
+        print("Hotkey gravada = ",Hotkey2)
+        HotkeyFile.write("\n" + Hotkey2)
+
+        # Gravando Hotkey 0
+        print("\nDigite a Hotkey que deseja gravar para zerar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+        Hotkey0 = input()
+        print("Hotkey gravada = ",Hotkey0)
+        HotkeyFile.write("\n" + Hotkey0)
+        HotkeyFile.close()
+
+        # Loop para correção
+        system('cls')
+        print("AS Hotkeys gravadas são:\n" + Hotkey + " para +\n" + Hotkey2 + " para -\n" + Hotkey0 + " para zerar.\n")
+        x = input("Deseja alterar as Hotkeys salvas? ('S' p/ SIM)\n\t")
