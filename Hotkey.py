c0aa81be59971924b261e625559a249c5b91b1a7
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2022-11-20 20:23:19 -0300

    1

--- Hotkey.py
+++ Hotkey.py
@@ -2,7 +2,36 @@
 # -*- coding: UTF-8 -*-
 from os import system
 from pynput import keyboard
+from Interface import jane
 
+
+    
+def altera():
+        # Gravando Hotkey +
+    HKs=("Digite a Hotkey que deseja gravar para aumentar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+    Hotkey = input()
+    HKs=("Hotkey gravada = ",Hotkey)
+    HotkeyFile = open("Hotkey.txt", "w")
+    HotkeyFile.write(Hotkey)
+
+    # Gravando Hotkey -
+    HKs=("\Digite a Hotkey que deseja gravar para reduzir a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+    Hotkey2 = input()
+    HKs=("Hotkey gravada = ",Hotkey2)
+    HotkeyFile.write("\n" + Hotkey2)
+
+    # Gravando Hotkey 0
+    HKs=("\Digite a Hotkey que deseja gravar para zerar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+    Hotkey0 = input()
+    HKs=("Hotkey gravada = ",Hotkey0)
+    HotkeyFile.write("\n" + Hotkey0)
+    HotkeyFile.close()
+
+    # Loop para correção
+    system('cls')
+    HKs=("AS Hotkeys gravadas são:\n" + Hotkey + " para +\n" + Hotkey2 + " para -\n" + Hotkey0 + " para zerar.")
+
+"""
 try:
     open("Hotkey.txt", "x")
 
@@ -14,7 +43,7 @@ except:
     HKp = HKp[:-1]  #Limpando o '\n' das linhas em "Hotkey.txt"
     HKm = HKm[:-1]
 
-    print("AS Hotkeys gravadas são:\n" + HKp + " para +\n" + HKm + " para -\n" + HKz + " para zerar. \n")
+    print("As Hotkeys gravadas são:\n" + HKp + " para +\n" + HKm + " para -\n" + HKz + " para zerar. \n")
     x = input("Deseja alterar as Hotkeys salvas? ('S' p/ SIM)\n\t")
     
 else:
@@ -48,3 +77,4 @@ finally:
         system('cls')
         print("AS Hotkeys gravadas são:\n" + Hotkey + " para +\n" + Hotkey2 + " para -\n" + Hotkey0 + " para zerar.\n")
         x = input("Deseja alterar as Hotkeys salvas? ('S' p/ SIM)\n\t")
+"""
--- Interface.py
+++ Interface.py
@@ -1,16 +1,68 @@
 # Interface
-# -*- coding: UTF-8 -*-
+# -*- coding: Windows_1252 -*-
 
 from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import *
 import Hotkey
-import Contador
+#import Contador
+
+def AltHK():
+    #HKs_orientacao["text"] = ""
+    Texto_HKin = Label(janela, text="aaAAA")
+    Texto_HKin.grid(column=0, row=3, padx=0, pady=10)
+    Caixa_HK = Entry(janela, width=53, )
+    Caixa_HK.grid(column=1, row=3, padx=10, pady=10)
+
+    Bt = Button(janela, text="Próximo", command=AltHK1)
+
+    # Gravando Hotkey +
+    HKs_orientacao["text"] =("Digite a Hotkey que deseja gravar para aumentar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+    
+
+def AltHK1():
+    
+    Hotkey = Caixa_HK.get()
+    HKs_orientacao["text"]=("Hotkey gravada = ",Hotkey)
+    HotkeyFile = open("Hotkey.txt", "w")
+    HotkeyFile.write(Hotkey)
+    
+    # Gravando Hotkey -
+    HKs=("\Digite a Hotkey que deseja gravar para reduzir a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+    Hotkey2 = Caixa_HK.get()
+    HKs=("Hotkey gravada = ",Hotkey2)
+    HotkeyFile.write("\n" + Hotkey2)
+
+    # Gravando Hotkey 0
+    HKs=("\Digite a Hotkey que deseja gravar para zerar a contagem.\nUse o modelo : ' <ctrl>+<shift>+x '")
+    Hotkey0 = Caixa_HK.get()
+    HKs=("Hotkey gravada = ",Hotkey0)
+    HotkeyFile.write("\n" + Hotkey0)
+    HotkeyFile.close()
+
+    # Loop para correção
+    
+    HKs=("AS Hotkeys gravadas são:\n" + Hotkey + " para +\n" + Hotkey2 + " para -\n" + Hotkey0 + " para zerar.")
+
+
 
 janela = Tk()
 janela.title("")
 #janela.resizable(width='false', height='false')
-HKbut = Button(janela, text='HK', command=Hotkey)
-HKbut.pack()
+HKbut = Button(janela, text='Alterar Hotkeys', command=AltHK)
+HKbut.grid(column=1, row=4, padx=10, pady=10)
+Caixa_HK = Entry(janela, width=53 )
+Caixa_HK.grid(column=1, row=3, padx=10, pady=10)
+
+with open('Hotkey.txt','r') as f:
+        HKp = f.readline()
+        HKm = f.readline()
+        HKz = f.readline()
+        HKp = HKp[:-1]  #Limpando o '\n' das linhas em "Hotkey.txt"
+        HKm = HKm[:-1]
+
+HKs = ("As Hotkeys gravadas são:\n" + HKp + " para +\n" + HKm + " para -\n" + HKz + " para zerar.")
 #asd
+HKs_orientacao = Label(janela, text=HKs, relief="solid")
+HKs_orientacao.grid(column=1, row=2, padx=20, pady=2)
 janela.mainloop()
