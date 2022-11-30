b3df5fcad0be1a2d65b82256d60d251a0048a931
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2022-11-30 17:45:23 -0300

    alteraçao de HKs

--- Nova_Interface.py
+++ Nova_Interface.py
@@ -1,36 +1,68 @@
 # Interface
-# -*- coding: Windows_1252 -*-
+# -*- coding: UTF-8 -*-
 
 from tkinter import *
 
-class Frames(Frame):
+class FrameHK(Frame):
     def __init__(self, master):
         super().__init__()
-    # Definir formatação das frames
+        self['bg'] = 'light blue'
+        self['bd'] = 5
+        self['relief'] = SUNKEN
+        self['padx'] = 30
+        self['pady'] = 8
+        self['height'] = 50
 
-class linhaHKs(Label):
-    def __init__(self, master):
-        super().__init__()
-    # Definir formatação das linhas de alteração das HKs
+
+def ConcluirHKs():          # Função salvar.
+    with open('Hotkey.txt','w') as f:
+        f.write(HKp.get() +"\n"+ HKm.get() +"\n"+ HKz.get())
+
 
 # Janela
 janela = Tk()
 janela.title("")
 
+with open('Hotkey.txt','r') as f:
+        HKp2 = f.readline()
+        HKm2 = f.readline()
+        HKz2 = f.readline()
+        HKp2 = HKp2[:-1]  #Limpando o '\n' das linhas em "Hotkey.txt"
+        HKm2 = HKm2[:-1]
+
+# Atribuindo as strings para as StringVars
+HKp = StringVar()
+HKp.set(HKp2)
+HKm = StringVar()
+HKm.set(HKm2)
+HKz = StringVar()
+HKz.set(HKz2)
+HKp3 = StringVar()
+HKp3.set("AaAa") 
+
 # Frame HKs
-FrameHKs = Frame(janela, 
-                 background="light blue",
-                 borderwidth=5,
-                 relief="sunken",
-                 padx=30,
-                 pady=8)
-TituloHKs = Label(FrameHKs, text="HOTKEYS GRAVADAS")
-TituloHKs.pack()
+FrameHKs = FrameHK(janela)
+TituloHKs = Label(FrameHKs, text="HOTKEYS GRAVADAS")    # modificar formatação usar 2 colunas
+TituloHKs.grid(row=0, column=0)
+
+HKp_label = Label(FrameHKs, text="Cont+1 : ").grid(row=1)          # Formatar as linhas
+HKp_entry = Entry(FrameHKs, textvariable=HKp).grid(column=1, row=1)
+
+HKm_label = Label(FrameHKs, text="Cont-1 : ").grid(row=2)
+HKm_entry = Entry(FrameHKs, textvariable=HKm).grid(column=1, row=2)
+
+HKz_label = Label(FrameHKs, text="Cont=0 : ").grid(row=3)
+HKz_entry = Entry(FrameHKs, textvariable=HKz).grid(column=1, row=3)
+
+ConcluirButton = Button(FrameHKs, text="ConcluirHKs", command=ConcluirHKs).grid(row=4, column=2)
+
+# Acrescentar opção para alterar as HK desabilitando td a frame
+
 FrameHKs.grid(column=0, row=0, padx=10, pady=2)
 
 # Frame Contador
-FrameCont = Frames(janela)
-FrameCont.grid(column=1, row=0,padx=10, pady=2)
+FrameCont = FrameHK(janela)
+FrameCont.grid(column=1, row=0, padx=10, pady=2)
 
 # Geometry
 TelaX = janela.winfo_screenwidth()
