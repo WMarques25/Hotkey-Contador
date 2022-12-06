d2e60baf095da2de2a13f6069aeed1ab6049981c
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2022-12-06 00:05:14 -0300

    # bug idiota do .grid

--- Nova_Interface.py
+++ Nova_Interface.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from tkinter import *
+from tkinter import ttk
 import logging
 
 logging.basicConfig(filename="LogCont.log", format="%(message)s")
@@ -16,17 +17,22 @@ class FrameHK(Frame):
         self['pady'] = 8
         self['height'] = 50
         
-        
-        
+
 def ConcluirHKs():          # Função salvar.
     with open('Hotkey.txt','w') as f:
         f.write(HKp.get() +"\n"+ HKm.get() +"\n"+ HKz.get())
+    
     # Desabilitar Frame inteira + Botão externo para Habilitar
     for child in FrameHKs.winfo_children():
         child.configure(state='disabled')       # Funcionando, Fazer o  mesmo pra ligar a edição
-    #FrameHKs.configure(state='disabled')
+    AltButton.configure(state='normal')
 
-
+def AltHKs():               # Função para alterar as Hks
+    for child in FrameHKs.winfo_children():
+        child.configure(state='normal')
+    AltButton.configure(state='disabled')
+    
+    
 ## Janela
 janela = Tk()
 janela.title("")
@@ -45,8 +51,6 @@ HKm = StringVar()
 HKm.set(HKm2)
 HKz = StringVar()
 HKz.set(HKz2)
-HKp3 = StringVar()
-HKp3.set("AaAa") 
 
 ## Frame HKs
 FrameHKs = FrameHK(janela)
@@ -54,26 +58,39 @@ FrameHKs.grid(column=0, row=0, padx=5, pady=2)
 
 TituloHKs = Label(FrameHKs, text="HOTKEYS GRAVADAS",
                   pady=5, font="Arial 15", bg="#52b6d1",
-                  bd=1, relief="solid", padx=5).grid(row=0, column=1, columnspan=2) 
+                  bd=1, relief="solid", padx=5, state='disabled').grid(row=0, column=1, columnspan=2) 
 
 HKp_label = Label(FrameHKs, text="Cont+1 : ", bg="#52b6d1",
                   bd=1, relief="solid", padx=2, anchor='w', 
-                  font="Arial 12").grid(row=1, sticky="EW")          
-HKp_entry = Entry(FrameHKs, textvariable=HKp, font="Arial 12").grid(column=1, row=1, columnspan=2, sticky="EW")
+                  font="Arial 12")
+HKp_label.grid(row=1, sticky="EW")          
+
+HKp_entry = Entry(FrameHKs, textvariable=HKp, font="Arial 12", state='disabled')
+HKp_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
 
 HKm_label = Label(FrameHKs, text="Cont-1  : ", bg="#52b6d1",
                   bd=1, relief="solid", padx=2, anchor='w', 
-                  font="Arial 12").grid(row=2, sticky="EW")
-HKm_entry = Entry(FrameHKs, textvariable=HKm, font="Arial 12").grid(column=1, row=2, columnspan=2, sticky="EW")
+                  font="Arial 12")
+HKm_label.grid(row=2, sticky="EW")
+
+HKm_entry = Entry(FrameHKs, textvariable=HKm, font="Arial 12", state='disabled')
+HKm_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
 
 HKz_label = Label(FrameHKs, text="Cont=0 : ", bg="#52b6d1",
                   bd=1, relief="solid", padx=2, anchor='w', 
-                  font="Arial 12").grid(row=3, sticky="EW")
-HKz_entry = Entry(FrameHKs, textvariable=HKz, font="Arial 12").grid(column=1, row=3, columnspan=2, sticky="EW")
+                  font="Arial 12")
+HKz_label.grid(row=3, sticky="EW")
 
-ConcluirButton = Button(FrameHKs, text="Concluir", command=ConcluirHKs, pady=2, bg="#52b6d1").grid(row=4, column=2, sticky=E, pady=3)
+HKz_entry = Entry(FrameHKs, textvariable=HKz, font="Arial 12", state='disabled')
+HKz_entry.grid(column=1, row=3, columnspan=2, sticky="EW")
+
+ConcluirButton = Button(FrameHKs, text="Concluir", command=ConcluirHKs, pady=2, bg="#52b6d1", state='disabled').grid(row=4, column=2, sticky=E, pady=3)
 
     # Acrescentar opção para alterar as HK desabilitando td a frame e a contagem.
+    # # Colocar o .grid na mesma linha de declaraçao da variavel inpede o uso do metodo .configure
+AltButton = Button(janela, text="Alterar", command=AltHKs, pady=2, bg="#52b6d1", state= NORMAL)
+AltButton.grid(row=1, column=0, sticky=E, pady=1, padx=10)
+
 
 ## Frame Contador
 FrameCont = FrameHK(janela)
